5c469de04cf7956212ff896029ed8a6a0434b76c
HasanTalukderIctian <hasantalukdercou@gmail.com>
2026-02-18 12:22:46 +0600

    Product Nav added and connection with Item Section

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,59 +1,72 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import { useContext } from "react";
 import { CartContext } from '../frontend/CartContext';
 import '../../assets/css/header.scss';
 
- // import your context
-
 const Header = () => {
-    const { cartItems } = useContext(CartContext); // get cart items
+
+    const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+
+    const handleProductClick = () => {
+        navigate("/", { state: { scrollTo: "items" } });
+    };
 
     return (
-        <>
-            <header className="sticky-header">
-    <div className='container py-3'>
-        <Navbar expand="lg">
-            <Navbar.Brand href="#home" className='logo'>
-                <span>Gazi</span> Builders
-            </Navbar.Brand>
+        <header className="sticky-header">
+            <div className='container py-3'>
+                <Navbar expand="lg">
+                    <Navbar.Brand href="/" className='logo'>
+                        <span>Gazi</span> Builders
+                    </Navbar.Brand>
 
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ms-auto">
-                    <Nav.Link href="/" className='nav-link' style={{ fontSize: "18px" }}>Home</Nav.Link>
-                    <Nav.Link href="/about" className='nav-link' style={{ fontSize: "18px" }}>About Us</Nav.Link>
-                    <Nav.Link href="/blogs" className='nav-link' style={{ fontSize: "18px" }}>Blogs</Nav.Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/" className='nav-link'>Home</Nav.Link>
+                            <Nav.Link href="/about" className='nav-link'>About Us</Nav.Link>
 
-                    <Nav.Link href="/cart" className="nav-link" style={{ fontSize: "18px", position: "relative" }}>
-                        <i className="bi bi-cart-fill me-2" style={{ color: "green", fontSize: "18px" }}></i> Cart
-                        {cartItems.length > 0 && (
-                            <span
-                                style={{
-                                    position: "absolute",
-                                    top: "-5px",
-                                    right: "-10px",
-                                    background: "red",
-                                    color: "#fff",
-                                    borderRadius: "50%",
-                                    padding: "2px 6px",
-                                    fontSize: "12px",
-                                    fontWeight: "bold"
-                                }}
+                            {/* ✅ Product scroll করবে */}
+                            <Nav.Link 
+                                onClick={handleProductClick} 
+                                className='nav-link'
+                                style={{ cursor: "pointer" }}
                             >
-                                {cartItems.length}
-                            </span>
-                        )}
-                    </Nav.Link>
-                    <Nav.Link href="/contact" className='nav-link'>Contact</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-    </div>
-</header>
+                                Product
+                            </Nav.Link>
 
-        </>
+                            <Nav.Link href="/blogs" className='nav-link'>Blogs</Nav.Link>
+
+                            <Nav.Link href="/cart" className="nav-link" style={{ position: "relative" }}>
+                                <i className="bi bi-cart-fill me-2" style={{ color: "green" }}></i> Cart
+                                {cartItems.length > 0 && (
+                                    <span
+                                        style={{
+                                            position: "absolute",
+                                            top: "-5px",
+                                            right: "-10px",
+                                            background: "red",
+                                            color: "#fff",
+                                            borderRadius: "50%",
+                                            padding: "2px 6px",
+                                            fontSize: "12px",
+                                            fontWeight: "bold"
+                                        }}
+                                    >
+                                        {cartItems.length}
+                                    </span>
+                                )}
+                            </Nav.Link>
+
+                            <Nav.Link href="/contact" className='nav-link'>Contact</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+        </header>
     );
 }
 
